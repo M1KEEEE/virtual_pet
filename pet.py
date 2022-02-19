5e6e894125b918b7b641da5246939a9060bd80f0
M1KEEEE <72807912+M1KEEEE@users.noreply.github.com>
2022-02-19 11:22:14 +0300

    Add files via upload

--- pet.py
+++ pet.py
@@ -1,25 +1,36 @@
 class Pet:
     def __init__(
-        self, 
-        name, 
-        fullnes, 
-        health, 
-        mood, 
-        cleannes, 
-        energy
+        self,
+        name="неизвестно",
+        fullness=0,
+        health=0,
+        mood=0,
+        cleanness=0,
+        energy=0
     ):
+        self.portrait = "assets/calm.png"
         self.name = name
-        self.fullnes = fullnes
+        self.fullness = fullness
         self.health = health
         self.mood = mood
-        self.cleannes = cleannesnes
+        self.cleanness = cleanness
         self.energy = energy
-        self.fullnes_change = -1
+        self.fullness_change = -1
         self.health_change = -1
         self.mood_change = -1
-        self.cleannes_change = -1
+        self.cleanness_change = -1
         self.energy_change = -1
 
     def feed(self, num):
-        self.fullnes += num
-        print("self")
+        self.fullness += num
+        print(f"{self.name} съел {num} и теперь у него {self.fullness} сытости")
+
+    def change_portrait(self):
+        pass
+
+    def decrease_stats(self):
+        self.fullness += self.fullness_change
+        self.health += self.health_change
+        self.mood += self.mood_change
+        self.cleanness += self.cleanness_change
+        self.energy += self.energy_change
